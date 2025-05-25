11fe67cda6a1a27fbfdc0577b6a2e0e5d14fd12b
WAcAW9 <wacaw9@gmail.com>
2025-05-25 20:51:37 +0900

    aw commit_first

---
+++ SchoolProject_KTK_CAW/src/main/java/dto/Article.java
@@ -0,0 +1,59 @@
+package dto;
+
+import java.io.Serializable;
+// 게시글 클래스
+public class Article implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	private String title;// 제목
+	private String user;// 작성자
+	private String description;// 설명
+	private String articleId; //아이디
+	
+	public Article() {
+		super();
+	}
+	
+	public Article(String title,String user) {
+		this.title=title;
+		this.user=user;
+		
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getArticleId() {
+		return articleId;
+	}
+
+	public void setArticleId(String articleId) {
+		this.articleId = articleId;
+	}
+
+
+}
